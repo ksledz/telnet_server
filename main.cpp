2fded5cf7c613a052056599398093e16942e1f6a
ksledz <kamila.ktos.sledz@gmail.com>
2018-09-20 12:57:10 +0200

    refactor 1

--- main.cpp
+++ main.cpp
@@ -16,25 +16,20 @@
 
 #define BUFFER_SIZE   2000
 #define QUEUE_LENGTH 5
+#define SINGLE_KEY_MODE  "\377\375\042\377\373\001"
+#define CLEAR_SCREEN  "\033[2J\033[H"
+#define BACK1  "\033["
+#define BACK2  "D"
 
 
-std::string clear_menu = "\377\375\042\377\373\001";
-std::string back_1 = "\033[";
-std::string back_2 = "D";
-
-enum ClientState {
-    a, b, end, b1, b2, back
-};
 enum Signal {
     up, down, enter, trash
 };
-enum Action {
-    nothing, disconnect, A, B1, B2
-};
+
 
 std::string print_with_enter(const std::string &s) {
     std::stringstream ss;
-    ss << s << "\n" << back_1 << (s.size() + 2) << back_2;
+    ss << s << "\n" << BACK1 << (s.size() + 2) << BACK2;
     return ss.str();
 }
 
@@ -45,7 +40,7 @@ public:
     struct sockaddr_in client_address;
     socklen_t client_address_len;
     char buffer[BUFFER_SIZE];
-    ssize_t len, snd_len;
+    ssize_t len;
     bool connected;
 
     TCPSocket(uint16_t PORT_NUM) {
@@ -120,14 +115,13 @@ public:
         return move;
 
     }
-    MenuOption (std::string name, int move = 0, std::string printed = "", bool disconnect = false, TCPSocket* sock = NULL) :
+
+    MenuOption (std::string name, int move = 0, std::string printed = "", bool disconnect = false, TCPSocket* sock = nullptr) :
     move(move),
     printed(std::move(printed)),
     disconnect(disconnect),
     name(std::move(name)),
     sock(sock) {}
-
-
 
 };
 
@@ -148,16 +142,17 @@ public:
     int add_option(unsigned long screen_number, const MenuOption &option) {
         if (screen_number > screens.size()) return 1;
         screens[screen_number].push_back(option);
+        return 0;
     }
 
     void reset_menu() {
         menu = 0;
         option = 0;
     }
+
     void print_menu() {
-        //sock->send(clear_menu);
-        sock->send("\033[2J");
-        sock->send("\033[H");
+        sock->send(CLEAR_SCREEN);
+
         int i = 0;
         for (auto moption: screens[menu]) {
             if (i == option) {
@@ -204,92 +199,9 @@ public:
 };
 
 
-Signal what_signal(const char *buf, ssize_t len) {
-    if (len < 2 || len > 3) return trash;
-    if (len == 2 && buf[0] == 13 && buf[1] == 0) return enter;
-    if (len == 3 && buf[0] == 27 && buf[1] == 91) {
-        if (buf[2] == 65) return up;
-        if (buf[2] == 66) return down;
-    }
-    return trash;
-}
 
-void print_space(TCPSocket *sock, ClientState state, int i) {
-    if (state == i) {
-        sock->send("*");
-    } else {
-        sock->send(" ");
-    }
-}
 
-void print_menu(TCPSocket *msg_sock, ClientState state) {
 
-    msg_sock->send("\033[2J");
-    msg_sock->send("\033[H");
-    if (state < 3) {
-        print_space(msg_sock, state, 0);
-        msg_sock->send(print_with_enter("Opcja A"));
-        print_space(msg_sock, state, 1);
-        msg_sock->send(print_with_enter("Opcja B"));
-        print_space(msg_sock, state, 2);
-        msg_sock->send(print_with_enter("Koniec"));
-
-    } else {
-        print_space(msg_sock, state, 3);
-        msg_sock->send(print_with_enter("Opcja B1"));
-        print_space(msg_sock, state, 4);
-        msg_sock->send(print_with_enter("Opcja B2"));
-        print_space(msg_sock, state, 5);
-        msg_sock->send(print_with_enter("Wstecz"));
-    }
-
-}
-
-void change_state(Signal signal, ClientState *state, Action *action) {
-    int temp_s = *state;
-    bool menu2 = false;
-    if (*state >= 3) menu2 = true;
-
-    if (signal == down) {
-        temp_s++;
-    }
-    if (signal == up) {
-        temp_s += 2;
-    }
-    if (signal < 2) {
-        temp_s %= 3;
-        if (menu2) temp_s += 3;
-    }
-    (*state) = static_cast<ClientState>(temp_s);
-    if (signal == enter) {
-        switch (*state) {
-            case a: {
-                *action = A;
-                break;
-            }
-            case b: {
-                *state = b1;
-                break;
-            }
-            case end: {
-                *action = disconnect;
-                break;
-            }
-            case b1: {
-                *action = B1;
-                break;
-            }
-            case b2: {
-                *action = B2;
-                break;
-            }
-            case back: {
-                *state = a;
-                break;
-            }
-        }
-    }
-}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -309,46 +221,15 @@ int main(int argc, char *argv[]) {
     ui.add_option(1, MenuOption("Wstecz", -1, "", false, &tcpsocket));
 
     for (;;) {
-        ClientState client = a;
-        Action action;
+
         tcpsocket.accept_connection();
-        tcpsocket.send(clear_menu);
-        //print_menu(&tcpsocket, client);
+        tcpsocket.send(SINGLE_KEY_MODE);
         ui.reset_menu();
         ui.print_menu();
 
         do {
             ui.react_to_signal(tcpsocket.receive_signal());
-            /*action = nothing;
-            //tcpsocket.receive();
-            change_state(tcpsocket.receive_signal(), &client, &action);
-
-            print_menu(&tcpsocket, client);
-
-            switch (action) {
-                case A: {
-                    tcpsocket.send("A");
-                    break;
-                }
-                case B1: {
-                    tcpsocket.send("B1");
-                    break;
-                }
-                case B2: {
-                    tcpsocket.send("B2");
-                    break;
-                }
-                case disconnect: {
-                    tcpsocket.len = -1;
-                    break;
-                }
-                case nothing: {
-                    break;
-                }
-            }*/
-
         } while (tcpsocket.connected);
-        //tcpsocket.end_connection();
     }
     return 0;
 }
